0ca90b674d9b9d54872b8f9e30e374157b11f3cf
Nauseouskibbles <greyson229@gmail.com>
2021-02-16 21:18:57 -0600

    basic setup

---
+++ basicflasksetup/routes.py
@@ -0,0 +1,6 @@
+from flask import render_template, url_for, redirect
+from basicflasksetup import app, db
+# from basicflasksetup.forms
+# from basicflasksetup.models
+from flask_login import login_user, current_user, logout_user, login_required
+
